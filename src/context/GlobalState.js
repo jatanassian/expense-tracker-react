43d6d52835be72d5047bc78d651340a681a417cb
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 10:16:24 -0500

    create a file that will contain our context and our reducer, and add dummy transactions in the initial state

---
+++ src/context/GlobalState.js
@@ -0,0 +1,12 @@
+import React, { createContext, useReducer } from 'react';
+
+// Initial state
+const initialState = {
+  // We only need the transactions. As long as we have the transactions data, we can pass them down to the components which then perform the calculations for the balance, etc.
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+  ]
+};
