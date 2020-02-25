49d9e6bc7d2abacb250b2d9cbda5b74a11ce8e87
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 11:42:51 -0500

    create an onSubmit function that create a transaction object and call the addTransaction action from the context with the new transaction passed to it

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount // Turn the amount into a number as otherwise it would be added as a string and would make the reduce crash
+    };
+
+    addTransaction(newTransaction);
+  }
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
@@ -16,6 +31,7 @@ export const AddTransaction = () => {
           <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
+        <button className="btn">Add transaction</button>
       </form>
     </>
   )
