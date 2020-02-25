cba5e31ae6aaf03c52a42f9cd82aa8cc189b698a
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 10:23:06 -0500

    create a Provider component that will wrap the other components (children) and provide them with the state or any action

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -13,3 +13,12 @@ const initialState = {
 
 // Create context
 export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer([AppReducer, initialState]);
+
+  return (<GlobalContext.Provider>
+    {children}
+  </GlobalContext.Provider>)
+}
