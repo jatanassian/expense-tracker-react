c3cc8536f8549b16cdd18b4d1086e5273e2d518e
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 10:27:43 -0500

    create the reducer, that for now only contains the default action type

---
+++ src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// A reducer ow we specify the application's state changes in response to certain actions to our context
+
+export default (state, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
