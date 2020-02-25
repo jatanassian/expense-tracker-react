d249e479c6d8740d50d6c66920309c3cc6be372e
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 10:57:44 -0500

    pass the GlobalContext to the component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     // The empty tag works as a Fragment
     <> 
