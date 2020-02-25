26e762497dcdf560287737ad2e36ff87c8e72afb
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-25 11:03:42 -0500

    calculate the total balance and make the component output it

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,11 +4,14 @@ import { GlobalContext } from '../context/GlobalState';
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
+  const amounts = transactions.map(t => t.amount); // Map through the context to create an array of all the amounts
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2); // Add all the amounts together
+
   return (
     // The empty tag works as a Fragment
     <> 
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </>
   )
 }
