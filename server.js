f164b1884037aaea7037a1fd28f96ca25fc6e02b
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-27 21:13:23 -0500

    add comments

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 
+// Let dotenv know where the config file is
 dotenv.config({ path: './config/config.env' });
 
+// Initialize the express app
 const app = express();
 
+// Route
 app.get('/', (req, res) => res.send('Hello'));
 
+// Run the server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
