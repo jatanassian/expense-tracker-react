6a51d823265319976ab66438b6a52da487707d5c
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-28 18:58:18 -0500

    only ignores the .env file instead of the whole config folder to avoid ignoring the database file

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// Connect to our database
+
+const mongoose = require('mongoose');
+
+// Any calls with mongoose to connect, find or create return a promise so we use async/await
+const connectDB = async () => {
+  try {
+    // Connection variable that returns a promise
+    // The second parameter is an object with some properties to stop some of the warnings mongoose might give us
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+  } catch (error) {
+    console.log(`Error: ${err.message}`.red);
+    process.exit(1); // Exit with failure, the application shuts down.
+  }
+}
+
+module.exports = connectDB;
