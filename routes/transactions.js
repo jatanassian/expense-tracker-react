dae64dac79aadd4ed6661a1a90e634cef0b807aa
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-27 21:30:16 -0500

    import the getTransactions method from the controller and implement it in the get request

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const { getTransactions } = require('../controllers/transactions');
 
-router.get('/', (req, res) => res.send('Hello'));
+router
+  .route('/')
+  .get(getTransactions);
 
 module.exports = router;
