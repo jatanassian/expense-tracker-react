9ad50c8b557f3a19c54f174d7a402a13c84cc8af
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-29 14:59:15 -0500

    import the connectDB function in the server and call it

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Let dotenv know where the config file is
 dotenv.config({ path: './config/config.env' });
+
+// Connect to the database
+connectDB();
 
 const transactions = require('./routes/transactions');
 
