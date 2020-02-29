4f22b1483e534b88fe08faf9307111cb8af9bb0f
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-29 15:34:45 -0500

    implement the getTransactions method with the Transaction model to get all the transactions in the db

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,22 +1,37 @@
 // This file will contain methods that will use the model to interact with our database
 
+const Transaction = require('../models/Transaction');
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return res.send(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 }
 
 // @desc Add all transactions
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('POST transactions');
 }
 
 // @desc Delete all transactions
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transactions');
 }
