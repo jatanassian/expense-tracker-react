d8b02d272ddee1b352d8fee5676e6d855329d6e1
Julien Atanassian <julien.atanassian@gmail.com>
2020-02-29 15:27:58 -0500

    create a Transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// When we make a request and send data, the db is only gonna accept data corresponding to this model (object with text and amount properties)
+
+const TransactionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add some text']
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Please add a positive or negative number']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
