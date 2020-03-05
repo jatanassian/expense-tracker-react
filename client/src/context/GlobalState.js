feda2cdf1fe4420a4b051387bd73aee5d9c70913
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-05 17:55:24 -0500

    create a getTransactions function that makes an axios get request to our api

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 const initialState = {
   // We only need the transactions. As long as we have the transactions data, we can pass them down to the components which then perform the calculations for the balance, etc.
   transactions: [],
-  error: null,
-  loading: true
+  error: null, // Put the error message in our state in case we want to display an alert
+  loading: true // To be able to use a spinner while it's loading
 };
 
 // Create context
@@ -18,6 +18,23 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions that are gonna make calls to our reducer
+  async function getTransactions() {
+    try {
+      const res = await axios.get('/api/v1/transactions');
+
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data
+      });
+
+    } catch (error) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: error.response.data.error
+      });
+    }
+  }
+
   const deleteTransaction = (id) => {
     dispatch({
       type:'DELETE_TRANSACTION',
