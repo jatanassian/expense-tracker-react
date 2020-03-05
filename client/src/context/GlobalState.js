7687418dba9f92429bcd52d91b9f9323feef7b47
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-05 17:52:45 -0500

    remove the initial transactions are we are now connecting the db to our app

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,15 +1,13 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios';
 
 // Initial state
 const initialState = {
   // We only need the transactions. As long as we have the transactions data, we can pass them down to the components which then perform the calculations for the balance, etc.
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ]
+  transactions: [],
+  error: null,
+  loading: true
 };
 
 // Create context
