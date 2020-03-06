bb836304d90515db928a59b029de82a2f3b1456e
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-05 20:06:36 -0500

    import the getTransactions function and implement it inside a useEffect hook

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,9 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // The empty array below prevents the infinite loop. This will fire off a warning in the console, if we want to quiet it down we can use eslint-disable-next-line react-hooks/exhaustive-deps which is in the useEffect documentation
+  }, []);
 
   return (
     <>
